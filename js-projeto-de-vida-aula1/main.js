52c5f3269d425a6398b300e6b5d15ac828390428
luizaanaaaa <ana.luiza.santos01@escola.pr.gov.br>
2025-03-25 10:19:03 -0300

    25/03

--- js-projeto-de-vida-aula1/main.js
+++ js-projeto-de-vida-aula1/main.js
@@ -22,7 +22,10 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempos[0]);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -31,6 +34,7 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+    if (tempoFinal > 0) {
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
     segundos %= 60;
